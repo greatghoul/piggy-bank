41844bdbdd4cea56475df7bcd2d36bc653cf318b
greatghoul <greatghoul@gmail.com>
2013-07-01 01:44:18 +0800

    完善 bonus 格式显示

--- filters.py
+++ filters.py
@@ -8,3 +8,8 @@ def escapejs(s):
     p = r'''(\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"'])'''
     r = lambda m: JS_ESCAPE_MAP.get(m.group())
     return s and re.sub(p, r, s, flags=re.I|re.M|re.X) or ''
+
+
+def dateformat(value, format="%Y-%m-%d %H:%M:%S"):
+    return value.strftime(format)
+
--- piggybank.py
+++ piggybank.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import uuid, json, re
+import json, re, time, datetime
 
 from flask import Flask, request, render_template, make_response, url_for, jsonify
 from sae.kvdb import KVClient
-from filters import escapejs
+from filters import escapejs, dateformat
 
 kv = KVClient()
 
@@ -12,22 +12,24 @@ app = Flask(__name__)
 app.debug = True
 
 app.jinja_env.filters['escapejs'] = escapejs
+app.jinja_env.filters['dateformat'] = dateformat
 
 @app.route('/', methods=['GET'])
 def home():
-    bonus_list = [json.loads(bonus) for _, bonus in kv.get_by_prefix('bonus-greatghoul')]
-    app.logger.info(bonus_list)
+    bonus_list = [bonus for _, bonus in kv.get_by_prefix('bonus-greatghoul')]
     return render_template('index.html', bonus_list=bonus_list)
 
 @app.route('/bonus', methods=['POST'])
 def add_bonus():
-    bonus_key = 'bonus-greatghoul-%s' % uuid.uuid4().hex 
+    timestamp = time.time()
+    bonus_key = 'bonus-greatghoul-%i' % (1000 * timestamp)
     bonus = dict(label=request.form.get('label'),
                  content=request.form.get('content'),
-                 bonus=int(request.form.get('bonus')))
-    bonus_value = json.dumps(bonus)
+                 bonus=int(request.form.get('bonus')),
+                 timestamp=datetime.datetime.fromtimestamp(timestamp))
+    # bonus_value = json.dumps(bonus)
 
-    kv.add(bonus_key, bonus_value)
+    kv.add(bonus_key, bonus)
 
     response = make_response(render_template('add_bonus.js', bonus=bonus))
     response.headers['Content-Type'] = 'text/javascript'
