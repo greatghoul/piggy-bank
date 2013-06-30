5f15cbb347639309d476742634bb48426ea966cb
greatghoul <greatghoul@gmail.com>
2013-06-30 17:10:59 +0800

    使用 flask 搭建基本页面

--- piggybank.py
+++ piggybank.py
@@ -3,8 +3,9 @@
 from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def home():
-    return 'Hello'
+    return render_template('index.html')
 
