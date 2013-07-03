4a300bf59290bc622a2ed5e6a4f08c436f625703
greatghoul <greatghoul@gmail.com>
2013-07-03 18:11:14 +0800

    添加配置文件

--- piggybank.py
+++ piggybank.py
@@ -2,17 +2,25 @@
 
 import json, re, time, datetime
 
-from flask import Flask, request, render_template, make_response, url_for, jsonify
+from flask import Flask, request, render_template, make_response, \
+                  url_for, jsonify, session, redirect
 from sae.kvdb import KVClient
 from filters import escapejs, dateformat
 
-kv = KVClient()
+instance_path = os.path.dirname(__file__)
 
 app = Flask(__name__)
-app.debug = True
+app.config.from_pyfile(os.path.join(instance_path, 'config.cfg'), silent=True)
 
 app.jinja_env.filters['escapejs'] = escapejs
 app.jinja_env.filters['dateformat'] = dateformat
+
+kv = KVClient()
+
+def get_referer():
+    """ 获取中转地址 """
+
+    return request.headers.get('HTTP_REFERER', request.url_root)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -34,4 +42,16 @@ def add_bonus():
     response = make_response(render_template('add_bonus.js', bonus=bonus))
     response.headers['Content-Type'] = 'text/javascript'
     return response
-    
+
+@app.route('/login')
+def login():
+    session['login_ok_url'] = get_referer()
+    callback = url_for('callback', _external=True) 
+
+    auth = OAuthHandler(consumer_key, consumer_secret, callback)
+    # Get request token and login url from the provider
+    url = auth.get_authorization_url()
+    session['oauth_request_token'] = auth.request_token
+    # Redirect user to login
+    return redirect(url)
+
